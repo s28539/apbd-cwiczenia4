af1127307290a79747089ac358092dff0a126d63
Mikołaj Solarz <s28539@pjwstk.edu.pl>
2024-04-08 21:48:07 +0200

    added AddUser_ReturnsFalseWhenEmailWithout_At_And_Dot()

--- zadanie/LegacyApp.Tests/UserServiceTests.cs
+++ zadanie/LegacyApp.Tests/UserServiceTests.cs
@@ -7,15 +7,28 @@ public class UserServiceTests
     public void AddUser_ReturnsFalseWhenFirstNameIsEmpty()
     {
         //Arrange
-        var userServive = new UserService();
+        var userService = new UserService();
 
         //Act
-        var result = userServive.AddUser(
-            null,"Kowalski","kowalski@gmial.com",
+        var result = userService.AddUser(
+            null,"Kowalski","kowalski@gmail.com",
             DateTime.Parse("2000-01-01"),
             1);
 
         //Assert
         Assert.False(result);
+    }
+
+    [Fact]
+    public void AddUser_ReturnsFalseWhenEmailWithout_At_And_Dot()
+    {
+        //Arrange
+        var userService = new UserService();
+        
+        //Act
+        var result = userService.AddUser("Jan", "Kowalski", "kowal", DateTime.Parse("2000-01-01"), 2);
+        
+        //Assert
+        Assert.Equal(false,result);
     }
 }
